69fb66fa2b49edaf5e4e3f2987364b48b2d3ee7f
Boreley <noaha.bearden@gmail.com>
2014-02-03 03:46:44 -0600

    First Class

---
+++ Black_Out/src/com/visual/Display.java
@@ -0,0 +1,15 @@
+package com.visual;
+
+import javax.swing.JFrame;
+
+public class Display {
+	
+	
+	public static void main(String[] args) {
+		
+		Display game = new Display();
+		JFrame frame = new JFrame(); 
+		
+	}
+
+}
